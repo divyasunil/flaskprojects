749f3b352a69b11dcf3be3e2b009f4f01ea66293
divya.sunil <divyaindu13@gmail.com>
2024-02-20 16:53:01 +0400

    flask

--- admin.py
+++ admin.py
@@ -10,7 +10,24 @@ def index():
 
 @app.route('/profile/<username>')
 def profile(username):
-    return render_template('profile.html',username=username)
+    return render_template('profile.html', username=username, isActive=True)
+
+
+@app.route('/names')
+def name():
+    names = ['Divya', 'Deepak', 'Rudra', 'Rohith']
+    return render_template('name.html', names=names)
+
+
+@app.route('/books')
+def book():
+    books = [
+        {'bookname': 'Anna Karenina', 'author': 'Leo Tolstoy', 'cover': 'https://images-na.ssl-images-amazon.com/images/I/51htC41jnKL.jpg'},
+        {'bookname': 'Madame Bovary', 'author': 'Gustave Flaubert', 'cover': 'https://pictures.abebooks.com/inventory/22732803445.jpg'},
+        {'bookname': 'The Great Gatsby', 'author': 'F. Scott Fitzgerald', 'cover': 'https://i.ebayimg.com/images/g/zxQAAOSwS1pl0ses/s-l500.jpg'},
+        {'bookname': 'The Adventures of Huckleberry Finn', 'author': ' Mark Twain', 'cover': 'https://i.pinimg.com/originals/f1/35/0b/f1350b9e5ced0a0fb5515949bedeeddb.jpg'},
+    ]
+    return render_template('book.html', books=books)
 
 
 app.run(debug=True)
